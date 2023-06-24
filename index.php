dba8dc8108d7f65808289f7d4fede676521e05e3
Harsheys21 <jha.harsh21@gmail.com>
2023-06-24 08:24:46 -0700

    version 1.2 minor changes

--- index.php
+++ index.php
@@ -120,7 +120,7 @@
                     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                         <div class="carousel-inner">
                             <div class="carousel-item active" data-bs-interval="10000">
-                            <img src="./photos/IMG_5572.png" class="d-block w-100" alt="...">
+                            <img src="./photos/IMG_8410.jpg" class="d-block w-100" alt="...">
                             </div>
                         <div class="carousel-item" data-bs-interval="2000">
                             <img src="./photos/pic10.png" class="d-block w-100" alt="...">
